d2cafa3886229832175cf0c0fa4758d04e75b41a
SaidLeon04 <122801569+SaidLeon04@users.noreply.github.com>
2023-01-24 14:06:46 +0000

    CREAR porgrama2.py para formatear el tipo de una variable entera y hacer posible concatenarla con una cadena de caracteres

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 |--|--|--|
 |1|programa0.py|Hola Mundo en Python3|
 |2|programa1.py|Comentarios multilinea, unilinea y concatenar dos cadenas|
+|3|programa2.py|.format para cambiar tipos de variables|
---
+++ semana2/programa2.py
@@ -0,0 +1,10 @@
+"""
+    programa2.py
+    Nombre: Said Isaac León Lara
+    Fecha: 24/01/2023
+    Descripción: formatear el tipo de una variable para cambiarlo a String usando .format
+"""
+variable1 = "Hola"  #  variable para almacenar una cadena de caráteres
+variable2 = 619  #  variable para almacenar una cadena de carácteres
+#print(variable1 + variable2)  #  concatenación de dos cadenas
+print("{}{}" .format(variable1, variable2))  #  formatear las variables cambiando su tipo
