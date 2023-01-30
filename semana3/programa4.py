5d374f67e3053096edbe00a7b88423e8c2fa4018
SaidLeon04 <122801569+SaidLeon04@users.noreply.github.com>
2023-01-30 14:54:48 +0000

    CREAR programa4.py utilizando la función casting para convertir una string a un entero

---
+++ semana3/programa4.py
@@ -0,0 +1,19 @@
+"""
+    programa4.py
+    Nombre: Said Isaac León Lara
+    Fecha: 30/01/2023
+    Descripción: Utilizando casting para convertir un string a int
+"""
+
+numero1 = int(input("Número 1: "))
+numero2 = 10
+suma = numero1 + numero2
+resta = numero1 - numero2
+division = numero1/numero2
+multiplicacion = numero1 * numero2
+potencia = numero1 ** numero2
+print ("La suma de ", numero1, "+", numero2, "es", suma)
+print ("La resta de ", numero1, "-", numero2, "es", resta)
+print ("La division de ", numero1, "/", numero2, "es", division)
+print ("La multiplicacion de ", numero1, "*", numero2, "es", multiplicacion)
+print ("La potencia de ", numero1, "^", numero2, "es", potencia)
