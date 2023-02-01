3600e1dbef0e00261018f39639095abdb4c5a2a6
SaidLeon04 <122801569+SaidLeon04@users.noreply.github.com>
2023-02-01 14:49:40 +0000

    CREAR programa7.py para comparar dos números e imprimir el mayor

--- README.md
+++ README.md
@@ -9,3 +9,4 @@
 |5|programa4.py|casting para cambiar un string a int
 |6|programa5.py|calcular el área y perimetro de cualquier triángulo (sin if)|
 |7|programa6.py|calcular el área y perímetro de un círculo y un cuadrado|
+|8|programa7.py|comparar 2 números entetros e imprimir el número mayor|
---
+++ semana3/programa7.py
@@ -0,0 +1,24 @@
+"""
+    programa7.py
+    Nombre: Said Isaac León Lara
+    Fecha: 1/02/2023
+    Descripción: Comparar 2 números entetros e imprimir el número mayor 
+"""
+
+numero1 = int(input("Número 1: ")) # lee un numero
+numero2 = int(input("Número 2: ")) # lee un numero
+"""
+if numero1 > numero2: # compara los dos numeros
+    print ("El número mayor es: ", numero1)
+elif numero2 > numero1: #segunda comparacion
+    print("El número mayor es: ", numero2)
+elif numero1 == numero2: # tercera comparacion
+    print(numero1, "y ", numero2, "son iguales")
+"""
+
+if numero1 > numero2: # compara los dos numeros
+    print (numero1)
+elif numero2 > numero1: #segunda comparacion
+    print(numero2)
+elif numero1 == numero2: # tercera comparacion
+    print(numero1)
