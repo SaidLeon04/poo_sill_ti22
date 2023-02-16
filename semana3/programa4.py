5b23037dfb12f4d8a0869a6744efff8e37859211
Said León <122801569+SaidLeon04@users.noreply.github.com>
2023-02-16 16:56:51 -0600

    COMENTAR programa4.py

--- semana3/programa4.py
+++ semana3/programa4.py
@@ -5,15 +5,15 @@
     Descripción: Utilizando casting para convertir un string a int
 """
 
-numero1 = int(input("Número 1: "))
-numero2 = 10
-suma = numero1 + numero2
-resta = numero1 - numero2
-division = numero1/numero2
-multiplicacion = numero1 * numero2
-potencia = numero1 ** numero2
-print ("La suma de ", numero1, "+", numero2, "es", suma)
-print ("La resta de ", numero1, "-", numero2, "es", resta)
-print ("La division de ", numero1, "/", numero2, "es", division)
-print ("La multiplicacion de ", numero1, "*", numero2, "es", multiplicacion)
-print ("La potencia de ", numero1, "^", numero2, "es", potencia)
+numero1 = int(input("Número 1: ")) # pide una entrada de tipo int que se asignará a la variable numero1
+numero2 = 10 # declara una variable llamada numero2 con un valor de 10
+suma = numero1 + numero2 # suma el valor en la variable numero1 mas numero2
+resta = numero1 - numero2 # resta el valor en la variable numero1 menos numero2
+division = numero1/numero2 # divide el valor en la variable numero1 entre numero2
+multiplicacion = numero1 * numero2  # multiplica el valor en la variable numero1 por numero2
+potencia = numero1 ** numero2 # eleva el valor en la variable numero1 a la potecia de numero2
+print ("La suma de ", numero1, "+", numero2, "es", suma) # imprime la suma de ambas variables y el resultado almacenado en la variable suma
+print ("La resta de ", numero1, "-", numero2, "es", resta) # imprime la resta de ambas variables y el resultado almacenado en la variable resta
+print ("La division de ", numero1, "/", numero2, "es", division) # imprime la división de ambas variables y el resultado almacenado en la variable division
+print ("La multiplicacion de ", numero1, "*", numero2, "es", multiplicacion) # imprime la multiplicación de ambas variables y el resultado almacenado en la variable multiplicacion
+print ("La potencia de ", numero1, "^", numero2, "es", potencia) # imprime la potencia de numero1 a la numero2 y el resultado almacenado en la variable potencia
