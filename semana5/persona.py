6ca96a30acc0a5759d30a65e5fbbbf6359c07ca1
Said León <122801569+SaidLeon04@users.noreply.github.com>
2023-02-16 17:43:29 -0600

    COMENTAR persona.py

--- semana5/persona.py
+++ semana5/persona.py
@@ -2,27 +2,27 @@
     persona.py
     Nombre: Said Isaac León Lara
     Fecha: 13/02/2023
-    Descripción: Crear un objeto llamado persona para usar los metodos get, set e init.
+    Descripción: Crear una clase llamada Persona con dos variable o atributos que obtendran un valor con la función set y lo devolvera con la función get
 """
 
-class Persona:
-    __nombre = None
-    __email = None
+class Persona: # Crea la clase Persona
+    __nombre = None # declara la variable privada __nombre con valor de None
+    __email = None # declara la variable privada __email con valor de None
     def __init__(self): # función que inicializa la clase
-        print("Persona")
-    def setNombre(self,nombre):
-        self.__nombre = nombre
-    def getNombre(self):
-        return self.__nombre
-    def setEmail(self,email):
-        self.__email = email
-    def getEmail(self):
-        return self.__email
+        print("Persona") # imprime el texto Persona
+    def setNombre(self,nombre): # función que asigna un valor a la variable nombre
+        self.__nombre = nombre # el valor de __nombre será igual nombre
+    def getNombre(self): # función que obtiene el valor de la variable nombre
+        return self.__nombre # retorna el valor de __nombre
+    def setEmail(self,email): # función que asigna un valor a la variable email
+        self.__email = email # el valor de __email será igual email
+    def getEmail(self): # función que obtiene el valor de la variable email
+        return self.__email # retorna el valor de __email
 
-dejah = Persona() # creación del objeto llamado dejah
+dejah = Persona() # crea un objeto de la clase Persona
 
-dejah.setNombre("Dejah") 
-print (dejah.getNombre())
+dejah.setNombre("Dejah") # usa el metodo setNombre para asignar un nombre al objeto
+print (dejah.getNombre()) #imprime la funcion getNombre para imprimir el nombre 
 
-dejah.setEmail("correo@mascorreo.correo") 
-print (dejah.getEmail())
+dejah.setEmail("correo@mascorreo.correo") # usa el metodo setEmail para asignar un email al objeto
+print (dejah.getEmail()) # imprime la funcion getEmail para imprimir el email 
